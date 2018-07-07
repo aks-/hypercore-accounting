c24cf0323bff57731ee79c8deb8c90b623497849
Ashok <coderatlabs@gmail.com>
2018-07-07 14:21:57 +0530

    format date for transactions

--- app/containers/list-transactions.js
+++ app/containers/list-transactions.js
@@ -1,9 +1,15 @@
-import   { connect } from 'react-redux'
+import { connect } from 'react-redux'
+import moment from 'moment-timezone'
 import ListTransactions from '../components/list-transactions'
 
 const mapStateToProps = state => ({
   screen: state.screen,
-  transactions: state.transactions
+  transactions: state.transactions.map(t => {
+    const date = moment.tz(t.date, null).format('LL')
+    return Object.assign({}, t, {
+      date
+    })
+  })
 })
 
 const ListTransactionsContainer = connect(
